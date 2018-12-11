34672760991c9ff94fbf954e71cd2ebf6a58edd2
Matthew Weaber <matthewweaber89@gmail.com>
2018-12-10 21:19:35 -0500

    Update 3

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,134 +1,116 @@
-var targetNumber;
 var comNumber;
-var button1Num = 0
+var button1Num = [];
 var button2Num = [];
 var button3Num = [];
 var button4Num = [];
 var counter = 0;
 var numberOptions = [];
+var wins = 0;
+var losses = 0;
 
 
+// $(".scoreToBeMatched").on("click", ".btn-active", function () {
+
+//     comNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+//     $("#scoreToMatch").text(comNumber);
+//     $(".buttonStart").attr("class", "buttonStart btn-disable");
+//     // console.log(".scoreToBeMatched");Generating number and update html
+
+// });
+
+function gameLost() {
+    $("#lossMade").text(losses);
+    $("#playerScore").text("");
+    counter = 0;
+}
+
+function gameWon() {
+    $("#winsMade").text(wins);
+    $("#playerScore").text("");
+    counter = 0;
+}
+
+function crystalNumbers() {
+
+    // function crystal1() {
+    button1Num = Math.round(Math.random() * (12 - 1) + 1);
+
+    // }
+
+    // function crystal2() {
+    button2Num = Math.round(Math.random() * (12 - 1) + 1);
+
+    // }
+
+    // function crystal3() {
+    button3Num = Math.round(Math.random() * (12 - 1) + 1);
+
+    // }
+
+    // function crystal4() {
+    button4Num = Math.round(Math.random() * (12 - 1) + 1);
+
+    // }
+}
 
 
+$(document).ready(function () {
+    crystalNumbers();
+    comNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    $("#scoreToMatch").text(comNumber);
 
-    $(".buttonStart").on("click", function() {
 
-        comNumber = Math.floor(Math.random() * (120 - 19) ) + 19;
+    $(".button1").on("click", function () {
+
+        counter += button1Num;
+        $("#playerScore").text(counter);
+        scoreChecker();
+
+    });
+
+    $(".button2").on("click", function () {
+
+        counter += button2Num;
+        $("#playerScore").text(counter);
+        scoreChecker();
+
+    });
+
+    $(".button3").on("click", function () {
+
+        counter += button3Num;
+        $("#playerScore").text(counter);
+        scoreChecker();
+
+    });
+
+    $(".button4").on("click", function () {
+
+        counter += button4Num;
+        $("#playerScore").text(counter);
+        scoreChecker();
+
+    });
+})
+
+function scoreChecker() {
+    if (counter == comNumber) {
+        wins++;
+        gameWon();
+        crystalNumbers();
+        comNumber = Math.floor(Math.random() * (120 - 19)) + 19;
         $("#scoreToMatch").text(comNumber);
-        
-           
-    })
-    function newGame(){
-        function crystal1(){
-            button1Num = Math.round(Math.random() * (12-1) + 1);
-            // button1Num = parseInt(button1Num);
-            return
-        }
-
-        function crystal2(){
-            button2Num = Math.round(Math.random() * (12-1) + 1);
-            // button1Num = parseInt(button1Num);
-            return
-        }
-
-        function crystal3(){
-            button3Num = Math.round(Math.random() * (12-1) + 1);
-            // button1Num = parseInt(button1Num);
-            return
-        }
-
-        function crystal4(){
-            button4Num = Math.round(Math.random() * (12-1) + 1);
-            // button1Num = parseInt(button1Num);
-            return
-        }
 
 
-        $(".button1").on("click", function(){
-       
-            counter+=button1Num;
-            $("#playerScore").text(counter);
-            if(counter == comNumber){
-                alert("you've won!");
-                newGame();
-                }else if (counter >= comNumber){
-                    alert("You've lost!");
-                    newGame();
-                }
-       
-
-        });
-
-        $(".button2").on("click", function(){
-        
-            counter+=button2Num;
-            $("#playerScore").text(counter);
-            if(counter == comNumber){
-                alert("you've won!");
-                newGame();
-                }else if (counter >= comNumber){
-                    alert("You've lost!");
-                    newGame();
-                }
-
-        });
-    
-        $(".button3").on("click", function(){
-        
-            counter+=button3Num;
-            $("#playerScore").text(counter);;
-            if(counter == comNumber){
-                alert("you've won!");
-                newGame();
-                }else if (counter >= comNumber){
-                    alert("You've lost!");
-                    newGame();
-                }
-                 
-                
-        
-
-        });
-
-        $(".button4").on("click", function(){
-        
-            counter+=button4Num;
-            $("#playerScore").text(counter);
-            if(counter == comNumber){
-                alert("you've won!");
-                newGame();
-                }else if (counter >= comNumber){
-                    alert("You've lost!");
-                    newGame();
-                }
-        
-
-        });
-
-
-
-
-        crystal1();
-        crystal2();
-        crystal3();
-        crystal4();
+    } else if (counter >= comNumber) {
+        losses++;
+        gameLost();
+        crystalNumbers();
+        comNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+        $("#scoreToMatch").text(comNumber);
 
     }
-
-        if(counter == comNumber){
-        alert("you've won!");
-        newGame();
-        }
-
-        else if (counter <= comNumber){
-        alert("You've lost!");
-        newGame();
-        }
-    newGame();
-
-
-
-
+}
 
 
 
