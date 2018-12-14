58860e74cb9deacedd16de05469605961ced113e
Matthew Weaber <matthewweaber89@gmail.com>
2018-12-14 18:19:44 -0500

    Changed the game

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -7,6 +7,8 @@ var counter = 0;
 var numberOptions = [];
 var wins = 0;
 var losses = 0;
+var numberArray=[1,2,3,4,5,6,7,8,9,10,11,12];
+var numArray=[];
 
 function gameLost() {
     $("#lossMade").text(losses);
@@ -21,24 +23,32 @@ function gameWon() {
 }
 
 function crystalNumbers() {
+    // for (var i = 0; i < 4; i++){
+    //     var meatball = Math.floor((Math.random() * numberArray.length) + 0);
+    //     var banana = numberArray[meatball];
+    //     numArray.push(banana);
+    //     numberArray.splice(meatball, 1);
+    //     console.log(banana[i]);
+    // }
+    
 
     // function crystal1() {
-    button1Num = Math.round(Math.random() * (12 - 1) + 1);
+    button1Num = Math.round(Math.random() * (3 - 1) + 1);
 
     // }
 
     // function crystal2() {
-    button2Num = Math.round(Math.random() * (12 - 1) + 1);
+    button2Num = Math.round(Math.random() * (6 - 4) + 4);
 
     // }
 
     // function crystal3() {
-    button3Num = Math.round(Math.random() * (12 - 1) + 1);
+    button3Num = Math.round(Math.random() * (9 - 7) + 7);
 
     // }
 
     // function crystal4() {
-    button4Num = Math.round(Math.random() * (12 - 1) + 1);
+    button4Num = Math.round(Math.random() * (12 - 10) + 10);
 
     // }
 }
@@ -46,7 +56,7 @@ function crystalNumbers() {
 
 $(document).ready(function () {
     crystalNumbers();
-    comNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    comNumber = Math.floor(Math.random() * (120 - 55)) + 55;
     $("#scoreToMatch").text(comNumber);
 
 
